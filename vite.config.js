11fe143a52dcec6edb3e2474d6e52b49b9cd6766
SGORGONE <sebgorgone@gmail.com>
2025-08-28 15:43:49 -0400

    there is no prod

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    {
+      name: 'nojekyll',
+      closeBundle() {
+        const outDir = resolve(__dirname, 'docs')
+        writeFileSync(`${outDir}/.nojekyll`, '')
+        console.log('✅ Added .nojekyll to docs/')
+      },
+    },
+  ],
+  build: {
+    outDir: 'docs', 
+  },
   base: '/cloudlog-local/'
 })
